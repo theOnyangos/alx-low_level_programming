f714e45dd70567f57ded16b950d85845cae29080
theOnyangos <otienodennis29@gmail.com>
2022-09-12 11:21:51 -0700

    add two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,25 @@
+#include <main.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (success)
+ */
+int add(int, int);
+
+int main(void)
+{
+	int n;
+
+	n = add(89, 9);
+	_putchar("%d\n", n);
+
+	return (0);
+}
+
+int add(int, int)
+{
+	int result = int + int;
+
+	return (result);
+}
