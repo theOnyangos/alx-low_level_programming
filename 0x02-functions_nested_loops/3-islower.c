24951d8dc4150c066e7d05c9faae34f57bc496d6
theOnyangos <otienodennis29@gmail.com>
2022-09-12 09:23:42 -0700

    check for lowercse charactor function

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,30 @@
+#include <main.h>
+
+/**
+ * main - entry point
+ *
+ * Description: Function that checks for lowercase charactors
+ */
+int _islower(int c)
+{
+	int result;
+
+	if(islower(c))
+	{
+		result = 1;
+		return (result);	
+	} else {
+		result = 0;
+		return (result);
+	}
+}
+
+int main(void)
+{
+	int r;
+
+	r = _islower("H");
+	_putchar(r + "0");
+
+	return (0);
+}
