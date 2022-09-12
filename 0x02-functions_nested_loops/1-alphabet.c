efa9a63bdfbe36f2be3836401e113c4a7e3d2410
theOnyangos <otienodennis29@gmail.com>
2022-09-12 07:00:55 -0700

    functio for printing alphabets in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include <alx.h>
+
+/**
+ * main - entry point for compiler
+ *
+ * description: function for printing alphabets to lowercase
+ */
+void print_alphabet(void)
+{
+	char alphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+	_putchar (alphabet);
+	_putchar ("\n");
+}
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+
