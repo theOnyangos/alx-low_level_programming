4caf6afcde1e0d43f22db3402f63be40ec6f1e94
theOnyangos <otienodennis29@gmail.com>
2022-09-13 14:32:11 -0700

    print numbers

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,52 +1,38 @@
-#include <main.h>
+#include "main.h"
 #include <stdio.h>
 
 /**
- * main - entry point
- *
- * Return: Always 0 (success)
+ * print_to_98 - print all the numbers between the provided number and 98
+ * @n: integer to start at
+ * Return: Void.
  */
-void print_to_98(int n);
 
-int main(void)
-{
-	print_to_98(0);
-	print_to_98(98);
-	ptint_to_98(111);
-	print_to_98(81);
-	print_to_98(-10);
-
-	return (0);
-}
-
-void print_to_98(void)
+void print_to_98(int n)
 {
 	int i = 0;
 
 	if (n > 98)
 	{
-		for (i = n; i >= 98; i++)
+		for (i = n; i >= 98; i--)
 		{
 			if (i != n)
 			{
 				printf(", ");
 			}
-
 			printf("%d", i);
 		}
 	}
-	else 
+	else
 	{
-		for (i = n; i <= 98; i++)
+		for (i = n; i <= 98 ; i++)
 		{
 			if (i != n)
 			{
 				printf(", ");
 			}
-
 			printf("%d", i);
 		}
 	}
-
-	printf("\n");
+	putchar('\n');
 }
+
