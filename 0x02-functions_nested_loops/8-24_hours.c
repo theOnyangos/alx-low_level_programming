fa7dad91253b358055d2e015f71a13e289f6ddff
theOnyangos <otienodennis29@gmail.com>
2022-09-13 14:10:23 -0700

    24-hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,35 +1,27 @@
-#include <main.h>
+#include "main.h"
 
 /**
- * main - script entry point
+ * jack_bauer - function
  *
- * Description: Function for printing time in mins
+ * Return: always 0;
  */
-void jack_bauer(void);
 
-int main(void)
+
+oid jack_bauer(void)
 {
-	jack_bauer();
-
-	return (0);
-}
-
-void jack_bauer(void)
-{
-       	int hr;
+	int hr;
 	int min;
 
 	for (hr = 0; hr < 24; hr++)
 	{
 		for (min = 0; min < 60; min++)
 		{
-			_putchar((hr / 10) + "0");
-			_putchar((hr % 10) + "0");
-			_putchar(":");
-			_putchar((min / 10) + "0");
-			_putchar((min % 10) + "0");
-			_putchar("\n");
-		}	
+			_putchar((hr / 10) + '0');
+			_putchar((hr % 10) + '0');
+			_putchar(':');
+			_putchar((min / 10) + '0');
+			_putchar((min % 10) + '0');
+			_putchar('\n');
+		}
 	}
- 		 
 }
