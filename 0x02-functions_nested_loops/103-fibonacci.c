d765699d1bdc1239c7a4c3c0733fe90ce03a5761
theOnyangos <otienodennis29@gmail.com>
2022-09-14 01:07:04 -0700

    103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - entry point function
+ *
+ * description: fibonacci
+ *
+ * Return: return (0) (success)
+ */
+
+int main(void)
+{
+	unsigned long int limit_num = 400000;
+	unsigned long int total_sum = 0;
+	unsigned long int after = 1;
+	unsigned long int before = 0;
+	unsigned long int B = 0;
+
+	while (limit_num > B)
+	{
+		B = before + after;
+
+		if ((B % 2) == 0)
+		{
+			total_sum += B;
+		}
+
+		before = after;
+		after = B
+	}
+	printf("%li\n", total_sum);
+	return (0);
+}
