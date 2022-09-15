02ce9260f2bbd782b04e4ad0f3947a37315ffedc
theOnyangos <otienodennis29@gmail.com>
2022-09-15 12:15:04 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper - function
+ *
+ * Return: 1 if (c) is ippercase otherwise 0
+ *
+ * @c: letter being checked
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && <= 90)
+		return (1);
+	else
+		return (0);
+}
